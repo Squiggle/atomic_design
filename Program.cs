13df254902d33fe1525dfa64cc783ac3af6b70b8
Jonathan Gilroy-Powell <jonathan.gilroy-powell@and.digital>
2021-10-28 14:50:30 +0100

    Revert name

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace atomic_design_blazor
+namespace techexp_blazor
 {
   public class Program
   {
